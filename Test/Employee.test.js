68c9d7a890ac92ef98d565cc2ea08831f9dc3b2b
james-hardy <topsec115@gmail.com>
2021-02-16 16:55:36 -0500

    final changes

--- Test/Employee.test.js
+++ Test/Employee.test.js
@@ -3,28 +3,29 @@ const Employee = require('../lib/employee.js');
 describe("Employee", () => {
     describe("Initialization", () => {
         it("should create an object with a name if provided valid argument", () => {
-            const employee = new Employee("John Doe", 11, "johndoe@test.com");
-            expect(employee.name).toEqual("John Doe");
-            expect(employee.id).toEqual(11);
-            expect(employee.email).toEqual("johndoe@test.com");
+            const employee = new Employee("JohnDoe", "11", "JohnDoe@test.com");
+            expect(employee.name).toEqual("JohnDoe");
+            expect(employee.id).toEqual("11");
+            expect(employee.email).toEqual("JohnDoe@test.com");
         });
-        it("should throw an error if provided no arguments", () => {
+
+        it("should throw an error if provided no argument", () => {
             const employee = () => new Employee();
             expect(employee).toThrow(Error("No Parameters Given"));
         });
 
         it("should throw an error if name is not a string", () => {
-            const employee = () => new Employee();
+            const employee = () => new Employee(1,"11","JohnDoe@test.com");
             expect(employee).toThrow(Error("Expected a string"));
         });
 
-        it("should throw an error if id is not a number", () => {
-            const employee = () => new Employee();
-            expect(employee).toThrow(Error("Expected a number"));
+        it("should throw an error if id is not a string", () => {
+            const employee = () => new Employee("JohnDoe",11,"JohnDoe@test.com");
+            expect(employee).toThrow(Error("Expected a string"));
         });
 
         it("should throw an error if email is not a string", () => {
-            const employee = () => new Employee();
+            const employee = () => new Employee("JohnDoe","11",11);
             expect(employee).toThrow(Error("Expected a string"));
         });
     });
